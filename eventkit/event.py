a29c3b7cf95893ececb63541924eb2d36c78cfdb
Matt Stancliff <matt@genges.com>
2024-08-18 11:31:49 -0700

    Fix CI error with docs building
    Fixes:
    
    - Incorporates doc deps into poetry (poetry install --with dev,docs)
    - Removes manual version.py parsing to get version number
    - Fixes "no language" error in docs spec
    - Fixes doc syntax problem in docstring
    
    Docs can now be built using:
    
    poetry run sphinx-build -b html docs html

--- eventkit/event.py
+++ eventkit/event.py
@@ -276,7 +276,9 @@ class Event:
             import eventkit as ev
 
             ev.Timer(0.25, count=10).run()
-            ->
+
+        Outputs:
+
             [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5]
 
         .. note::
